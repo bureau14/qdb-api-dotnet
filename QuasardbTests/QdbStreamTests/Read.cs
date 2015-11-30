c32cfb194b14923ddfedff2c7bd4d8156bf898a6
Benoit Blanchon <benoit@quasardb.net>
2015-11-30 14:50:55 +0100

    Test read

--- QuasardbTests/QdbStreamTests/Read.cs
+++ QuasardbTests/QdbStreamTests/Read.cs
@@ -10,14 +10,14 @@ namespace QuasardbTests.QdbStreamTests
     public class Read
     {
         private Stream _stream;
+        private static readonly byte[] _content = Encoding.ASCII.GetBytes("Hello World!");
 
         [TestInitialize]
         public void Initiliaze()
         {
             _stream = QdbTestCluster.CreateAndOpenStream();
-            
-            var content = Encoding.ASCII.GetBytes("Hello World!");
-            _stream.Write(content, 0, content.Length);
+            _stream.Write(_content, 0, _content.Length);
+            _stream.Position = 0;
         }
 
         [TestCleanup]
@@ -27,7 +27,7 @@ namespace QuasardbTests.QdbStreamTests
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ObjectDisposedException), "Cannot access a closed Stream.")]
+        [ExpectedException(typeof(ObjectDisposedException))]
         public void ThrowsObjectDisposedException()
         {
             _stream.Close();
@@ -35,7 +35,7 @@ namespace QuasardbTests.QdbStreamTests
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentNullException), "Buffer cannot be null.Parameter name: buffer.")]
+        [ExpectedException(typeof(ArgumentNullException))]
         public void ThrowsArgumentNullException_WhenBufferIsNull()
         {
             // ReSharper disable once AssignNullToNotNullAttribute
@@ -43,32 +43,55 @@ namespace QuasardbTests.QdbStreamTests
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException), "Non-negative number required.Parameter name: offset.")]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
         public void ThrowsArgumentOutOfRangeException_WhenOffsetIsNegative()
         {
             _stream.Read(new byte[2], -1, 4);
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException), "Non-negative number required.Parameter name: count.")]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
         public void ThrowsArgumentOutOfRangeException_WhenCountIsNegative()
         {
             _stream.Read(new byte[2], 4, -1);
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentException), "Offset and length were out of bounds for the array or count is greater than the number of elements from index to the end of the source collection.")]
+        [ExpectedException(typeof(ArgumentException))]
         public void ThrowsArgumentException_WhenSumOfOffsetAndLengthIsToBig()
         {
             _stream.Read(new byte[2], 1, 2);
         }
 
-        // TODO: need Seek
-        /*[TestMethod]
-        public void ReturnsNumberOfBytes()
+        [TestMethod]
+        public void ReturnsNumberOfBytes_Section()
         {
             var result = _stream.Read(new byte[2], 0, 2);
             Assert.AreEqual(2, result);
-        }*/
+        }
+
+        [TestMethod]
+        public void ReturnsNumberOfBytes_Whole()
+        {
+            var result = _stream.Read(new byte[100], 0, 100);
+            Assert.AreEqual(_content.Length, result);
+        }
+
+        [TestMethod]
+        public void FillsBuffer_Section()
+        {
+            var buffer = new byte[2];
+            _stream.Read(buffer, 0, 2);
+            Assert.AreEqual(_content[0], buffer[0]);
+            Assert.AreEqual(_content[1], buffer[1]);
+        }
+        [TestMethod]
+        public void FillsBuffer_Whole()
+        {
+            var buffer = new byte[100];
+            var len = _stream.Read(buffer, 0, 100);
+            Array.Resize(ref buffer, len);
+            CollectionAssert.AreEqual(_content, buffer);
+        }
     }
 }
