30444e8f3a5ea26acd7caa24eddb68d5d196069b
Marek Kurdej <marek@quasardb.net>
2017-01-26 14:44:35 +0100

    Fix copyright year.

--- QuasardbTests/Properties/AssemblyInfo.cs
+++ QuasardbTests/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@
 [assembly: AssemblyDescription("Unit tests for quasardb API for .NET")]
 [assembly: AssemblyCompany("quasardb SAS")]
 [assembly: AssemblyProduct("quasardb")]
-[assembly: AssemblyCopyright("Copyright © quasardb SAS 2015")]
+[assembly: AssemblyCopyright("Copyright © quasardb SAS 2017")]
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
