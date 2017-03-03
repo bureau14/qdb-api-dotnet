f05e8d9d902a9cc1d7461796bcd83aa656911a60
Benoit Blanchon <benoit@quasardb.net>
2017-03-03 17:53:40 +0100

    Don't throw when entry type is unknown

--- Quasardb/QdbEntryFactory.cs
+++ Quasardb/QdbEntryFactory.cs
@@ -44,8 +44,15 @@ namespace Quasardb
                     return new QdbTimeSeries(_handle, alias);
 
                 default:
-                    throw new NotImplementedException($"Entry type \"{type}\" not supported in {nameof(QdbEntryFactory)}. Please contact quasardb support.");
+                    return new QdbUnknownEntry(_handle, alias);
             }
+        }
+    }
+
+    class QdbUnknownEntry : QdbEntry
+    {
+        public QdbUnknownEntry(QdbApi api, string alias) : base(api, alias)
+        {
         }
     }
 }
