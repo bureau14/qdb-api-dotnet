01783acc43db464c0689e9dc28e33b1db618ad5c
Benoit Blanchon <benoit@quasardb.net>
2017-03-06 18:51:41 +0100

    Fix qdb_timespec epoch

--- Quasardb/TimeConverter.cs
+++ Quasardb/TimeConverter.cs
@@ -5,13 +5,16 @@ namespace Quasardb
 {
     static class TimeConverter
     {
+        static DateTime _epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
         const long NanosPerTick = 100;
         const long TicksPerSecond = 1000000000 / NanosPerTick;
 
         public static qdb_timespec ToTimespec(DateTime dt)
         {
+            var ticksSinceEpoch = (dt - _epoch).Ticks;
+
             qdb_timespec ts;
-            ts.tv_sec = Math.DivRem(dt.ToFileTimeUtc(), TicksPerSecond,
+            ts.tv_sec = Math.DivRem(ticksSinceEpoch, TicksPerSecond,
                 out ts.tv_nsec);
             ts.tv_nsec *= NanosPerTick;
             return ts;
@@ -19,7 +22,7 @@ namespace Quasardb
 
         public static DateTime ToDateTime(qdb_timespec ts)
         {
-            return DateTime.FromFileTimeUtc(ts.tv_sec * TicksPerSecond + ts.tv_nsec / NanosPerTick);
+            return _epoch.AddTicks(ts.tv_sec * TicksPerSecond + ts.tv_nsec / NanosPerTick);
         }
     }
 }
