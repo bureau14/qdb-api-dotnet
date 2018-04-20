cb14374151e8c57415fa712df1dc0a6ef506f2f0
Marek Kurdej <marek@quasardb.net>
2018-04-20 14:21:45 +0200

    [release] Automatically fix copyright year in script.

--- scripts/release/set_version.sh
+++ scripts/release/set_version.sh
@@ -10,9 +10,13 @@ fi
 INPUT_VERSION=$1; shift
 
 XYZB_VERSION=${INPUT_VERSION%%-*}
+CURRENT_YEAR=`date +%Y`
 
 cd $(dirname -- $0)
 cd ${PWD}/../..
+
+# [assembly: AssemblyCopyright("Copyright © quasardb SAS 2017")]
+sed -i -e 's/\[assembly: AssemblyCopyright("\([^"]*\) [0-9]\+")\]/[assembly: AssemblyCopyright("\1 '"${CURRENT_YEAR}"'")]/' Quasardb/Properties/AssemblyInfo.cs
 
 # [assembly: AssemblyVersion("2.1.0.0")]
 # [assembly: AssemblyFileVersion("2.1.0.0")]
