22e0fff7ff3c21276b858a612e94f58dabdca246
vianney <vianney.p@outlook.com>
2019-09-27 10:58:28 +0200

    Fix typo.

--- Quasardb.Tests/Tutorial.cs
+++ Quasardb.Tests/Tutorial.cs
@@ -25,60 +25,64 @@ namespace Quasardb.Tests.Tutorial
         [TestMethod]
         public void OnInsecure()
         {
-            // connect-start
-            QdbCluster c = new QdbCluster("qdb://127.0.0.1:2836");
-            // connect-end
-            Assert.IsNotNull(c);
+            if (!DaemonRunner.UseSecurity)
+            {
+                // connect-start
+                QdbCluster c = new QdbCluster("qdb://127.0.0.1:2836");
+                // connect-end
+                Assert.IsNotNull(c);
 
-            // create-table-start
-            // First we acquire a reference to a table (which may or may not yet exist)
-            var ts = c.TimeSeries("stocks");
+                // create-table-start
+                // First we acquire a reference to a table (which may or may not yet exist)
+                var ts = c.TimeSeries("stocks");
 
-            // Initialize our column definitions
-            var columns = new QdbColumnDefinition[]{
+                // Initialize our column definitions
+                var columns = new QdbColumnDefinition[]{
                     new QdbDoubleColumnDefinition("open"),
                     new QdbDoubleColumnDefinition("close"),
                     new QdbInt64ColumnDefinition("volume")};
 
-            // Now create the table with the default shard size
-            ts.Create(columns);
-            // create-table-end
+                // Now create the table with the default shard size
+                ts.Create(columns);
+                // create-table-end
 
-            // tags-start
-            ts.AttachTag(c.Tag("nasdaq"));
-            // tags-end
+                // tags-start
+                ts.AttachTag(c.Tag("nasdaq"));
+                // tags-end
 
-            // column-insert-start
-            // Prepare some data to be inserted
-            var opens = new QdbDoublePointCollection { { new DateTime(2019, 02, 01), 3.40 }, { new DateTime(2019, 02, 02), 3.50 } };
-            var closes = new QdbDoublePointCollection { { new DateTime(2019, 02, 01), 3.50 }, { new DateTime(2019, 02, 02), 3.55 } };
-            var volumes = new QdbInt64PointCollection { { new DateTime(2019, 02, 01), 10000 }, { new DateTime(2019, 02, 02), 7500 } };
+                // column-insert-start
+                // Prepare some data to be inserted
+                var opens = new QdbDoublePointCollection { { new DateTime(2019, 02, 01), 3.40 }, { new DateTime(2019, 02, 02), 3.50 } };
+                var closes = new QdbDoublePointCollection { { new DateTime(2019, 02, 01), 3.50 }, { new DateTime(2019, 02, 02), 3.55 } };
+                var volumes = new QdbInt64PointCollection { { new DateTime(2019, 02, 01), 10000 }, { new DateTime(2019, 02, 02), 7500 } };
 
-            // Retrieve the different columns from our timeseries
-            var openCol = ts.DoubleColumns["open"];
-            var closeCol = ts.DoubleColumns["close"];
-            var volumeCol = ts.Int64Columns["volumne"];
+                // Retrieve the different columns from our timeseries
+                var openCol = ts.DoubleColumns["open"];
+                var closeCol = ts.DoubleColumns["close"];
+                var volumeCol = ts.Int64Columns["volume"];
 
-            // Insert data for each column
-            openCol.Insert(opens);
-            closeCol.Insert(closes);
-            volumeCol.Insert(volumes);
-            // column-insert-end
+                // Insert data for each column
+                openCol.Insert(opens);
+                closeCol.Insert(closes);
+                volumeCol.Insert(volumes);
+                // column-insert-end
 
-            // column-get-start
-            // using the same columns we used for the insertion
-            // we can retrieve the points from a specific range
-            var range = new QdbTimeInterval(new DateTime(2019, 02, 01), new DateTime(2019, 02, 02));
+                // column-get-start
+                // using the same columns we used for the insertion
+                // we can retrieve the points from a specific range
+                var range = new QdbTimeInterval(new DateTime(2019, 02, 01), new DateTime(2019, 02, 02));
 
-            openCol.Points(range);
+                openCol.Points(range);
 
-            // you can now inspect the values in the enumerable Points
-            var resultPoints = openCol.Points();
-            // column-get-end
-            var ptEnum = resultPoints.GetEnumerator();
-            ptEnum.MoveNext();
-            Assert.AreEqual(ptEnum.Current.Time, opens[0].Time);
-            Assert.AreEqual(ptEnum.Current.Value, opens[0].Value);
+                // you can now inspect the values in the enumerable Points
+                var resultPoints = openCol.Points();
+                // column-get-end
+                var ptEnum = resultPoints.GetEnumerator();
+                ptEnum.MoveNext();
+                Assert.AreEqual(ptEnum.Current.Time, opens[0].Time);
+                Assert.AreEqual(ptEnum.Current.Value, opens[0].Value);
+                ts.Remove();
+            }
         }
     }
 }
