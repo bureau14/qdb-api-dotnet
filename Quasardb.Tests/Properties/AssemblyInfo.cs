df428839fe8d941e8361cfc0471cea58f3cfabcb
terngkub <terngnattapol@gmail.com>
2020-04-10 15:15:32 +0200

    Bump version to 3.9.0.0-nightly.0.

--- Quasardb.Tests/Properties/AssemblyInfo.cs
+++ Quasardb.Tests/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@
 [assembly: AssemblyCompany("quasardb SAS")]
 [assembly: AssemblyProduct("quasardb")]
 [assembly: AssemblyCopyright("Copyright (c) 2009-2020, quasardb SAS. All rights reserved.")]
-[assembly: AssemblyVersion("3.8.0.0")]
-[assembly: AssemblyFileVersion("3.8.0.0")]
+[assembly: AssemblyVersion("3.9.0.0")]
+[assembly: AssemblyFileVersion("3.9.0.0")]
