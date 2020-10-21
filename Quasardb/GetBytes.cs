f814e8e12899a8d230cab59160937af4dbedb435
Vianney Plotton <vianney.plotton@gmail.com>
2020-10-21 15:37:15 +0200

    QDB-3484 - Check for null values and empty blob or string. (#12)

---
+++ Quasardb/GetBytes.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace Quasardb
+{
+    /// <summary>
+    /// Helper class
+    /// </summary>
+    public static class Helper
+    {
+        /// <summary>
+        /// Safely Get Bytes
+        /// </summary>
+        public static byte[] GetBytes(IntPtr Pointer, int size)
+        {
+            // CAUTION: limited to 32 bits!!!
+            if (size == 0)
+                return null;
+            if (Pointer == IntPtr.Zero)
+                return null;
+            var buffer = new byte[size];
+            Marshal.Copy(Pointer, buffer, 0, size);
+            return buffer;
+        }
+    }
+
+}
