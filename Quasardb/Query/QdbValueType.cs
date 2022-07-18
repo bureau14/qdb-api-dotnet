fa6383c459eccc38c28fe83802c08e3b22640910
vianney <vianney.p@outlook.com>
2022-07-18 10:51:28 +0200

    QDB-10827 - Update query result format.

--- Quasardb/Query/QdbValueType.cs
+++ Quasardb/Query/QdbValueType.cs
@@ -43,6 +43,31 @@ namespace Quasardb.Query
         /// <summary>
         /// A result of type String />.
         /// </summary>
-        String = 5
+        String = 5,
+
+        /// <summary>
+        /// A result of type Double Array />.
+        /// </summary>
+        DoubleArray = 6,
+
+        /// <summary>
+        /// A result of type Int64 Array />.
+        /// </summary>
+        Int64Array = 7,
+
+        /// <summary>
+        /// A result of type Blob Array />.
+        /// </summary>
+        BlobArray = 8,
+
+        /// <summary>
+        /// A result of type Timestamp Array />.
+        /// </summary>
+        TimestampArray = 9,
+
+        /// <summary>
+        /// A result of type String Array />.
+        /// </summary>
+        StringArray = 10,
     }
 }
