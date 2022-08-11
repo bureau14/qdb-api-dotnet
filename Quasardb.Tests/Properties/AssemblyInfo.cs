74e2b08f80e8d18079b6324a3f909b26a574938d
vianney <vianney.p@outlook.com>
2022-08-11 17:27:59 +0200

    Set beta version.

--- Quasardb.Tests/Properties/AssemblyInfo.cs
+++ Quasardb.Tests/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@
 [assembly: AssemblyCompany("quasardb SAS")]
 [assembly: AssemblyProduct("quasardb")]
 [assembly: AssemblyCopyright("Copyright (c) 2009-2021, quasardb SAS. All rights reserved.")]
-[assembly: AssemblyVersion("3.15.0.0")]
-[assembly: AssemblyFileVersion("3.15.0.0")]
+[assembly: AssemblyVersion("3.15.0-beta.0")]
+[assembly: AssemblyFileVersion("3.15.0-beta.0")]
