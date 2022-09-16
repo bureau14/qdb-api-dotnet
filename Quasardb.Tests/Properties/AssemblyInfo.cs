afa56fe27a24dafd7d4decffaf8d0d7811a913be
Marek Kurdej <marek@quasardb.net>
2022-09-16 15:07:33 +0200

    Bump copyright year.

--- Quasardb.Tests/Properties/AssemblyInfo.cs
+++ Quasardb.Tests/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@
 [assembly: AssemblyDescription("Unit tests for quasardb API for .NET")]
 [assembly: AssemblyCompany("quasardb SAS")]
 [assembly: AssemblyProduct("quasardb")]
-[assembly: AssemblyCopyright("Copyright (c) 2009-2021, quasardb SAS. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright (c) 2009-2022, quasardb SAS. All rights reserved.")]
 [assembly: AssemblyVersion("3.15.0.0")]
 [assembly: AssemblyFileVersion("3.15.0.0")]
