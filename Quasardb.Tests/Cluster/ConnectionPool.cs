071a04c7891435f75f3841ecd0ac7502b2f1182f
vianney <vianney.p@outlook.com>
2022-10-05 10:53:00 +0200

    Implement connection pool.

---
+++ Quasardb.Tests/Cluster/ConnectionPool.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quasardb.Tests.Cluster
+{
+    [TestClass]
+    public class ConnectionPool
+    {
+        public QdbConnectionPool CreatePool(int size)
+        {
+
+            if (DaemonRunner.UseSecurity)
+            {
+                return new QdbConnectionPool(DaemonRunner.ClusterUrl, DaemonRunner.ClusterPublicKey,
+                    DaemonRunner.UserName, DaemonRunner.UserPrivateKey, size);
+            }
+            else
+            {
+                return new QdbConnectionPool(DaemonRunner.ClusterUrl, size);
+            }
+        }
+        [TestMethod]
+        public void CantTakeMoreThanLimit()
+        {
+            var pool = CreatePool(2);
+            var cs1 = pool.Get();
+            var cs2 = pool.Get();
+
+            var limit = new TimeSpan(0, 0, 1);
+            QdbCluster cs3;
+
+            // This one won't work
+            Assert.IsFalse(pool.TryGet(out cs3, limit));
+
+            var content = Encoding.UTF8.GetBytes($"Hello world!!!!");
+
+            var alias1 = RandomGenerator.CreateUniqueAlias();
+            cs1.Blob(alias1).Put(content);
+            var alias2 = RandomGenerator.CreateUniqueAlias();
+            cs2.Blob(alias2).Put(content);
+
+            pool.Return(cs1);
+
+            // After returning one connection we can get it
+            Assert.IsTrue(pool.TryGet(out cs3, limit));
+            CollectionAssert.AreEqual(cs3.Blob(alias1).Get(), content);
+        }
+    }
+}
