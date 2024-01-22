eec7b0e539230adba362eb01a558a490f153edb6
terngkub <terngnattapol@gmail.com>
2024-01-23 01:19:41 +0700

    Update copyright year.

--- Quasardb.Tests/Properties/AssemblyInfo.cs
+++ Quasardb.Tests/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@
 [assembly: AssemblyDescription("Unit tests for quasardb API for .NET")]
 [assembly: AssemblyCompany("quasardb SAS")]
 [assembly: AssemblyProduct("quasardb")]
-[assembly: AssemblyCopyright("Copyright (c) 2009-2023, quasardb SAS. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright (c) 2009-2024, quasardb SAS. All rights reserved.")]
 [assembly: AssemblyVersion("3.14.1.0")]
 [assembly: AssemblyFileVersion("3.14.1.0")]
--- Quasardb/Properties/AssemblyInfo.cs
+++ Quasardb/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyDescription("quasardb API for .NET")]
 [assembly: AssemblyCompany("quasardb SAS")]
 [assembly: AssemblyProduct("quasardb")]
-[assembly: AssemblyCopyright("Copyright (c) 2009-2023, quasardb SAS. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright (c) 2009-2024, quasardb SAS. All rights reserved.")]
 [assembly: AssemblyVersion("3.14.1.0")]
 [assembly: AssemblyFileVersion("3.14.1.0")]
 [assembly: CLSCompliant(true)]
