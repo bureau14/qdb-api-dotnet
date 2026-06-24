2bd817d9e6122ee40cb946d7eb424959de271d99
Igor <igor.niebylski@gmail.com>
2026-06-24 12:53:41 +0200

    revert

--- scripts/cicd/test.sh
+++ scripts/cicd/test.sh
@@ -15,7 +15,7 @@ pushd "${PROJECT_ROOT}"
 
 status=0
 
-cp -r Quasardb/linux/* Quasardb.Tests/bin/${BUILD_CONFIGURATION}/${DOTNET_FRAMEWORK}
+cp -r Quasardb/linux/ Quasardb.Tests/bin/${BUILD_CONFIGURATION}/${DOTNET_FRAMEWORK}
 # cp -r Quasardb/windows/* Quasardb.Tests/bin/${BUILD_CONFIGURATION}/${DOTNET_FRAMEWORK}
 
 set +e
