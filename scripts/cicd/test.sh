fbeeda3f92b682b1cefe51403cf12be69f328cf3
Igor <igor.niebylski@gmail.com>
2026-06-24 12:36:24 +0200

    hack :/

--- scripts/cicd/test.sh
+++ scripts/cicd/test.sh
@@ -15,6 +15,9 @@ pushd "${PROJECT_ROOT}"
 
 status=0
 
+cp -r Quasardb/linux Quasardb.Tests/bin/${BUILD_CONFIGURATION}/${DOTNET_FRAMEWORK}
+cp -r Quasardb/windows Quasardb.Tests/bin/${BUILD_CONFIGURATION}/${DOTNET_FRAMEWORK}
+
 set +e
 "${DOTNET}" vstest \
   "Quasardb.Tests/bin/${BUILD_CONFIGURATION}/${DOTNET_FRAMEWORK}/Quasardb.Tests.dll" \
