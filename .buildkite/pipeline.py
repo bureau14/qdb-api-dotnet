c52257ed687abdd74b103940ef4b68fbc15fe426
Igor <igor.niebylski@gmail.com>
2026-06-26 12:30:14 +0200

    updated agents

--- .buildkite/pipeline.py
+++ .buildkite/pipeline.py
@@ -58,8 +58,7 @@ GLOBAL_ENV: dict[str, str] = {
 }
 STEP_ENV: dict[str, dict[str, str]] = {}
 OS_ENV: dict[str, dict[str, str]] = {
-    # "linux": {"DOTNET_PATH": "$$QDB_CICD_AGENT_DOTNET"},
-    "linux": {"DOTNET_PATH": "/usr/share/dotnet/dotnet"},
+    "linux": {"DOTNET_PATH": "$$QDB_CICD_AGENT_DOTNET"},
     "windows": {},
 }
 OS_STEP_ENV: dict[str, dict[str, str]] = {}
