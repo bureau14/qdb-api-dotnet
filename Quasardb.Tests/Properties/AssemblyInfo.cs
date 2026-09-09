33dc1531bf814e0393cd5a8cd95a344086a88431
Igor <igor.niebylski@gmail.com>
2026-09-09 09:04:34 +0200

    Bump version to 3.14.3.

--- Quasardb.Tests/Properties/AssemblyInfo.cs
+++ Quasardb.Tests/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@
 [assembly: AssemblyCompany("quasardb SAS")]
 [assembly: AssemblyProduct("quasardb")]
 [assembly: AssemblyCopyright("Copyright (c) 2009-2024, quasardb SAS. All rights reserved.")]
-[assembly: AssemblyVersion("3.14.3.1")]
-[assembly: AssemblyFileVersion("3.14.3.1")]
+[assembly: AssemblyVersion("3.14.3.0")]
+[assembly: AssemblyFileVersion("3.14.3.0")]
